38f4c47a77798ac1f897119c40061316a710f4a8
sang <104421901+psawg@users.noreply.github.com>
2024-09-29 06:59:17 +0700

    update
    ffff

---
+++ assets/js/HomeJS.js
@@ -0,0 +1,17 @@
+function loadHTMLComponent(url, id){
+    fetch (url)
+    .then(response => response.text())
+    .then(html =>{
+        const elememt = document.getElementById(id)
+        if(elememt){
+            elememt.innerHTML = html;
+        }
+    })
+    .catch(error =>console.error(error))
+}
+
+loadHTMLComponent('/components/Footer.html', 'footer');
+loadHTMLComponent('/components/Header.html', 'header');
+loadHTMLComponent('/components/Section.html', 'section');
+
+
